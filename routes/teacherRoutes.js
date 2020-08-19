fc8bc1b022a2f48c40367714f2365bf26ca12226
sapnaa-rawat <69008976+sapnaa-rawat@users.noreply.github.com>
2020-08-19 23:15:43 +0530

    Add files via upload
    updated file upload

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,5 +1,32 @@
 var express = require('express');
 var router = express.Router();
+
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '../public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null,Date.now()+ file.originalname)
+  }
+})
+const fileFilter=(req, file, cb)=>{
+  if (file.mimetype==='image/jpeg'||file.mimetype==='image/jpg'||file.mimetype==='image/png'){
+    cb(null,true);
+  }else{
+    cb(null,false);
+  }
+}
+
+var upload = multer({
+  storage:storage,
+ /*limits:{
+    fileSize:1024*1024*1024*1024*1024*1024*5
+  },
+  fileFilter:fileFilter*/
+});
+
 function authToken(req, res, next) {
     try {
       const token = req.headers.authorization.split(" ")[1];
@@ -9,17 +36,18 @@ function authToken(req, res, next) {
       console.log(decode._doc);
       next();
     } catch (err) {
-      res.status(401).send("invalid token");
+      res.status(401).send("invalid t4oken");
     }
   }
 var user1=require('../modules/teacherRegister')
 var users1=require('../modules/teacherlogin')
-var course=require('../modules/addcourse')
+var course=require('../modules/addcourse.js')
 
 router.post('/signup',user1.signupteacher)
 router.post('/login',users1.loginuser)
 router.post('/addcourse',course.addCourses)
 router.post('/addstudent',course.addNewStudent)
-
+router.post('/uploadpdf',upload.single('coursePdf'),course.coursepdf)
+router.post('/uploadpdf/multiple',upload.array('coursePdf',13),course.coursepdf)
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,23 @@ var router = express.Router();
 
 var user=require('../modules/userRegister');
 var users=require('../modules/userlogin');
-var course=require('../modules/addcourse');
+var course=require('../modules/addcourse.js');
+
+
+/*
+router.get('/download', function (req, res, next) {
+    var filePath = "C:\Users\shree\Desktop\nodejsss\MAJORASSIGNMENT\myapp\public\download"; 
+    var fileName = "report.pdf"; // The default name the browser will use
+
+    res.download(filePath, fileName);    
+});*/
 
 //-------------------------------student----------------------------------------------------
 
 router.post('/signup',user.signupUser)
 router.post('/login',users.loginuser)
 router.post('/showcourses',course.showCourses)
+//router.post('/downloadpdf',download.single('coursePdf'),course.coursepdf)
 
 module.exports = router;
 
