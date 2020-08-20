e8231a3dd06654d2a01695ae6bd7d64a9002c3cb
Ankit Prasad <45434449+ankitprasad7@users.noreply.github.com>
2020-08-20 18:35:33 +0530

    Updated Student schema

--- models/student.js
+++ models/student.js
@@ -12,6 +12,7 @@ var Students = new Schema({
       unique: true,
     },
   },
+  marks: [{ courseID: { type: Number } }, { marksObtained: { type: Number } }],
   email: {
     type: String,
     required: true,
