91f0a6defdadc46a706adda610f0253ead2fd65a
Ankit Prasad <45434449+ankitprasad7@users.noreply.github.com>
2020-08-20 19:17:04 +0530

    added marks functionality

--- modules/addcourse.js
+++ modules/addcourse.js
@@ -1,6 +1,6 @@
 const Course = require("../models/Course");
 const mail = require("./mailer");
-
+const Students = require("../models/student");
 //for adding a new course
 
 function addCourses(req, res, next) {
@@ -55,37 +55,32 @@ function showCourses(req, res) {
   var studentEmail = req.body.studentemail;
 
   Course.find({ enrolledStudents: { $in: studentEmail } }).then((data) => {
-   // console.log(data);
+    // console.log(data);
     res.status(200).send(data);
   });
 }
 
-
 //for adding a coursepdf
 function coursepdf(req, res, next) {
-  
-  console.log(req.file);
+  var courseID = req.body.courseid;
+  // var studentEmail = req.body.studentemail;
 
-  let courseName = req.body.coursename;
-  let courseLecturer = req.body.courselecturer;
-  let coursedata = new Course({
-    coursename: courseName,
-    courselecturer: courseLecturer,
-  image: req.file.path
-  });
-  coursedata
-    .save()
-    .then((doc) => {
-      res.status(201).json({
-        message: "coursepdf added successfully",
-        results: doc,
-      });
+  Course.findOneAndUpdate({ courseid: courseID }, { image: req.file.path })
+    .then((data) => {
+      if (data.length < 1) {
+        res.send("already exist");
+      } else {
+        res.status(200).json({
+          message: "added successfully",
+          results: data,
+        });
+      }
     })
     .catch((err) => {
       res.json(err);
     });
 }
-//For updating marks 
+
 function addMarks(req, res) {
   var marksGiven = {
     courseID: req.body.courseid,
@@ -93,20 +88,21 @@ function addMarks(req, res) {
   };
   Students.findOneAndUpdate(
     { studentid: req.body.studentid },
-    { $addToSet: { marks: marksGiven } },{new:true}
-  ).then((data) => {
-    res.status(200).json({ message: "added successfully", results: data });
-  })
-  .catch((err)=>{
-    res.json(err);
-  });
+    { $addToSet: { marks: marksGiven } },
+    { new: true }
+  )
+    .then((data) => {
+      res.status(200).json({ message: "added successfully", results: data });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 }
-
 
 module.exports = {
   addCourses,
   addNewStudent,
   showCourses,
   coursepdf,
-  addMarks
+  addMarks,
 };
