254e32406f11abd7f97fd2be0cd46665f3972c81
Ankit Prasad <45434449+ankitprasad7@users.noreply.github.com>
2020-08-21 11:09:26 +0530

    added random password generator

--- modules/teacherRegister.js
+++ modules/teacherRegister.js
@@ -1,9 +1,56 @@
 var teachers = require('../models/teacher');
 //var mongoose=require('mongoose');
 var bcrypt = require('bcryptjs');
-
-
+var Students = require("../models/student");
+var mail = require("./mailer");
+function generatePassword() {
+  var length = 6,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 //SignUp user
+function signupUser(req, res, next) {
+  console.log(req.body);
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = generatePassword();
+  console.log(password);
+
+  bcrypt.hash(password, 10, function (err, hash) {
+    if (err) {
+      res.json({
+        message: "something is wrong",
+        error: err,
+      });
+    } else {
+      console.log(hash);
+      mail.sendingMail(email, password);
+      var data = new Students({
+        username: username,
+        email: email,
+        password: hash,
+      });
+      data
+        .save()
+        .then((doc) => {
+          res.status(200).json({
+            message: "user registerd successfully",
+            results: doc,
+          });
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    }
+  });
+}
+
+
+//SignUp Teacher
 function signupteacher(req, res, next) {
 console.log(req.body)
 var username=req.body.username;
